b4a113a265ae4dfee53e304067e0333d6e89bb83
Randy Gylling <MikeGylling@yahoo.com>
2016-04-11 14:35:07 -0700

    Removed class and finished last to do

--- starter-code 1/scripts/article.js
+++ starter-code 1/scripts/article.js
@@ -27,7 +27,7 @@ Article.prototype.toHtml = function() {
 
   $newArticle.append('<hr>');
 
-  // TODO: This cloned article is no longer a template, so we should remove that class...
+  $newArticle.removeClass('template');
 
   return $newArticle;
 }
