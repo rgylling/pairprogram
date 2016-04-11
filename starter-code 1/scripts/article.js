513e7dd9271494abeee1a25d6e0383ce53cc1812
Randy Gylling <MikeGylling@yahoo.com>
2016-04-11 14:33:18 -0700

    finished my function constructor

--- starter-code 1/scripts/article.js
+++ starter-code 1/scripts/article.js
@@ -1,9 +1,12 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the js object passed in to complete this contructor function:
-  // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorUrl = opts.autherUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
